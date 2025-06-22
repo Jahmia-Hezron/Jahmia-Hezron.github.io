704ee3b1bb3249728054171bd99c6f7ea5aa8e22
Jahmia Hezron <hezron.p.jahmia@gmail.com>
2025-06-22 19:38:53 +0300

    typo fixes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
-    // optionally:
     // basePath: '/hezronjahmiapresci.github.io',
     // assetPrefix: '/hezronjahmiapresci.github.io/',
     // trailingSlash: true
