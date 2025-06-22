ab497eadafdaeed06fd7200f3ae90b5e88cd3f49
Jahmia Hezron <hezron.p.jahmia@gmail.com>
2025-06-22 19:19:34 +0300

    adds next

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // ← Add this line
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
