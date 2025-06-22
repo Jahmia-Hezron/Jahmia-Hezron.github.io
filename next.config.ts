6a84cbb60a58ff4b3390d43e10a1002ce29cf175
Jahmia Hezron <hezron.p.jahmia@gmail.com>
2025-06-22 19:30:17 +0300

    configures next.congig.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+    // optionally:
+    // basePath: '/hezronjahmiapresci.github.io',
+    // assetPrefix: '/hezronjahmiapresci.github.io/',
+    // trailingSlash: true
+  
 };
 
 export default nextConfig;
